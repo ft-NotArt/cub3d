d9cc529ba34b441d987d28c2ea8bd6df0a397e7f
NotArt <noterisarthur42@gmail.com>
2025-02-03 17:42:48 +0100

    ~ | Azy

--- inc/cub3d.h
+++ inc/cub3d.h
@@ -1,18 +1,28 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cub3d.c                                            :+:      :+:    :+:   */
+/*   cub3d.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: anoteris <noterisarthur42@gmail.com>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/03 15:32:25 by kaveo             #+#    #+#             */
-/*   Updated: 2025/02/03 17:41:28 by anoteris         ###   ########.fr       */
+/*   Created: 2025/02/03 17:40:26 by anoteris          #+#    #+#             */
+/*   Updated: 2025/02/03 17:42:28 by anoteris         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "cub3d.h"
+#ifndef CUB3D_H
+# define CUB3D_H
 
-int	main()
-{
-	
-}
+# include "libft.h"
+# include "get_next_line.h"
+# include "MLX42/MLX42.h"
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <fcntl.h>
+# include <errno.h>
+# include <math.h>
+# include <stdbool.h>
+
+#endif
